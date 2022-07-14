56e151dcda287db0ee272368f0af0dcdc591ce4e
Your Name <you@example.com>
2022-07-15 00:08:48 +0100

    First commit

--- Js/index.js
+++ Js/index.js
@@ -14,37 +14,37 @@ convertBtn.addEventListener("click", function() {
 
 function meterToFeet(number) {
 
-    return `${number} meters = ${(number * 3.281).toFixed(3)} feet`;
+    return `${number} meters = ${(number * 3.28084).toFixed(3)} feet`;
 
 }
 
 function feetToMeter(number) {
 
-    return `${number} feet = ${(number / 3.281).toFixed(3)} meters`;
+    return `${number} feet = ${(number / 3.28084).toFixed(3)} meters`;
 
 }
 
 
 function litresToGallon(number) {
 
-    return `${number} litres = ${(number * 0.264).toFixed(3)} gallons`;
+    return `${number} litres = ${(number * 0.264172).toFixed(3)} gallons`;
 
 }
 
 function gallonToLitres(number) {
 
-    return `${number} gallons = ${(number / 0.264).toFixed(3)} litres`;
+    return `${number} gallons = ${(number / 0.264172).toFixed(3)} litres`;
 
 }
  
 function kgToPounds(number) {
 
-    return `${number} kg = ${(number * 2.204).toFixed(3)} pounds`;
+    return `${number} kilos = ${(number * 2.20462).toFixed(3)} pounds`;
 
 }
 
 function poundsToKg(number) {
 
-    return `${number} pounds = ${(number * 2.204).toFixed(3)} kg`;
+    return `${number} pounds = ${(number / 2.20462).toFixed(3)} kilos`;
 
 }
